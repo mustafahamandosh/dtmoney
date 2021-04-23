c228d64fa58141beb2a86eabcb521e071a8de94d
haman <hamandoshm@hotmail.com>
2021-04-23 02:13:55 -0300

    feat: add transaction table component

---
+++ src/components/TransactionTable/index.tsx
@@ -0,0 +1,38 @@
+import {TransactionTableContainer} from "./styles";
+
+export const TransactionTable = () => {
+    return (
+        <TransactionTableContainer>
+            <table>
+                <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Value</th>
+                    <th>Categories</th>
+                    <th>Data</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>Development</td>
+                    <td>R$12.000</td>
+                    <td>Programming</td>
+                    <td>20/02/2021</td>
+                </tr>
+                <tr>
+                    <td>Development</td>
+                    <td className="deposit">R$12.000</td>
+                    <td>Programming</td>
+                    <td>20/02/2021</td>
+                </tr>
+                <tr>
+                    <td>Development</td>
+                    <td className="withdraw">-R$12.000</td>
+                    <td>Programming</td>
+                    <td>20/02/2021</td>
+                </tr>
+                </tbody>
+            </table>
+        </TransactionTableContainer>
+    )
+}
