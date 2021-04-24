77a1a7219fdac01ac046c13ed1a74a6cf82a3e11
haman <hamandoshm@hotmail.com>
2021-04-24 02:30:43 -0300

    refactor: format currency and date

--- src/components/TransactionTable/index.tsx
+++ src/components/TransactionTable/index.tsx
@@ -37,9 +37,17 @@ export const TransactionTable = () => {
                                    }) =>
                     <tr key={id}>
                         <td>{title}</td>
-                        <td className={type}>{amount}</td>
+                        <td className={type}>
+                            {new Intl.NumberFormat('pt-BR',
+                                {
+                                    style: 'currency',
+                                    currency: 'BRL'
+                                }).format(amount)}
+                        </td>
                         <td>{category}</td>
-                        <td>{createdAt}</td>
+                        <td>{new Intl.DateTimeFormat('pt-BR')
+                            .format(new Date(createdAt))}
+                        </td>
                     </tr>
                 )}
                 </tbody>
